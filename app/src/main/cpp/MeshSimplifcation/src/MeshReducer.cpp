08871699ab53b71f883d431335668ce84a76363b
Meng Yan <mengyan@qti.qualcomm.com>
2020-04-15 15:20:32 +0800

    Remove unused variable and fix unreference issue

--- app/src/main/cpp/MeshSimplifcation/src/MeshReducer.cpp
+++ app/src/main/cpp/MeshSimplifcation/src/MeshReducer.cpp
@@ -216,8 +216,7 @@ public:
     }
 
     // priority = cost / (normal * tri_quality)
-    static double computePriority(const Vec3 &optPos, const SymetricMatrix &Q, const double &QuadricCost,
-                                  Vertex *start, Vertex *end)
+    static double computePriority(const Vec3 &optPos, const double &QuadricCost, Vertex *start, Vertex *end)
     {
         // replace the position with optPos
         Vec3 start_old = start->Pos;
@@ -264,7 +263,7 @@ public:
         minQual = std::min(minQual, 1.0);
 
         // cost
-        double cost = ScaleFactor * QuadricCos
+        double cost = ScaleFactor * QuadricCost;
         if (cost <= QUADRIC_EPSILON)
         {
             cost = -1 / (start_old - end_old).length();
@@ -343,7 +342,7 @@ public:
         SymetricMatrix Q = edge.Start->Q + edge.End->Q;
         double cost = 0.0;
         edge.OptPos = calcOptimalPosition(Q, edge.Start, edge.End, cost);
-        edge.Priority = computePriority(edge.OptPos, Q, cost, edge.Start, edge.End);
+        edge.Priority = computePriority(edge.OptPos, cost, edge.Start, edge.End);
     }
 
     static bool flipped(Vertex *start, Vertex *end, const Vec3 &optPos)
@@ -517,14 +516,14 @@ public:
         v0->Q += v1->Q;
         for (auto &v : VertexVec)
         {
+            // reset
+            v->NeedToUpdate = false;
+
             Edges.emplace_back(v0, v);
             solve(Edges.back());
             // update mark
             Edges.back().LOCAL_MARK = GLOBAL_MARK;
             std::push_heap(Edges.begin(), Edges.end());
-
-            // reset
-            v->NeedToUpdate = false;
         }
         return nDeleted;
     }
